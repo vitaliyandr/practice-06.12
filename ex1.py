39222b8dc43c3f0ff1e2f53f8e027f561a2c842b
vitaliyandr <116282877+vitaliyandr@users.noreply.github.com>
2022-12-06 17:41:04 +0200

    Add files via upload

--- ex1.py
+++ ex1.py
@@ -1,13 +1,5 @@
-try:
+begin = int(input("begin->"))
+end = int(input("end->"))
 
-    begin = int(input("begin->"))
-    end = int(input("end->"))
-
-    for i in range(begin, end+1):
-        print(i, end='\t')
-
-except ValueError as vl_ex:
-    print(f'Value error: {vl_ex}')
-except Exception as ex:
-    print(f'Error: {ex}')
-    print(f'Name: {ex.__class__.__name__}')
+for i in range(begin, end+1):
+    print(i, end='\t')
